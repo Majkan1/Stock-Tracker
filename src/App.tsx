6f437d2a295b68ff8f3634daaa5c2eb24c286bb8
Mikołaj Michalak <majkanart117@gmail.com>
2026-03-18 17:43:01 +0100

    Add react-router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-import './App.css'
-
+import { Routes,Route,Link} from "react-router-dom"
 function App() {
 
   return (
     <>
-    <p>Hej</p>
+    <Nav/>
     </>
   )
 }
 
+function Nav(){
+  return(
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/Search">Search</Link> 
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Search" element={<Search/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+function Home(){
+  return (
+    <>
+      <p>Hej</p>
+    </>
+  )
+}
+
+function Search(){
+  return(
+    <div>
+      <p>Search</p>
+    </div>
+  )
+}
 export default App
