3d6e11613c4a77678422f4aff1c174d5357aa30c
Mikołaj Michalak <majkanart117@gmail.com>
2026-03-19 18:04:05 +0100

    chore:Add a useStockStore.tsx.

---
+++ src/store/useStockStore.tsx
@@ -0,0 +1,13 @@
+//import {create} from 'zustand'
+
+/*interface : SingleAction {
+    name:string,
+    price:number,
+    change:number
+} 
+
+const useBear = create((set)=>({
+    bears:0
+    addStock: ()=> set((state)=>({bears:state.bears+1})),
+    removeStock: ()=> set({bears:0})
+}))*/
